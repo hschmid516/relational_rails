1e3c5abd2e135f8eaf9951952f9970871e1af1af
Laura Court <laura.mcourt@gmail.com>
2021-08-23 23:43:20 -0400

    Create routes for index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'home#index'
+  get '/world_bosses', to: 'world_bosses#index'
+  get '/regions', to: 'regions#index'
 end
