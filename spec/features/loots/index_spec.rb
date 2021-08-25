254c67fc0448e400868c7f138389fcacc035951b
Laura Court <laura.mcourt@gmail.com>
2021-08-24 23:03:57 -0400

    Interpolate data and add strings

--- spec/features/loots/index_spec.rb
+++ spec/features/loots/index_spec.rb
@@ -12,22 +12,35 @@ RSpec.describe "Loots has an index page", type: :feature do
     @mu_memory = @muckformed.loots.create!(name: "Memory of a Frenzied Monstrosity", memory: true, armor: 0)
     @feet = @muckformed.loots.create!(name: "Jingling Stone Stompers", memory: false, armor: 82)
     @gloves = @muckformed.loots.create!(name: "Hardened Castle Crusher", memory: false, armor: 21)
+    visit "/loots"
 
   end
 
   it 'can see all loots recorded in the system and attributes' do
-    visit "/loots"
-
-    expect(current_path).to eq("/loots")
     expect(page).to have_content(@ring.name)
     expect(page).to have_content(@cloth_belt.name)
     expect(page).to have_content(@m_memory.name)
     expect(page).to have_content(@mu_memory.name)
     expect(page).to have_content(@feet.name)
-    expect(page).to have_content(@gloves.updated_at)
-    expect(page).to have_content(@feet.world_boss.id)
-    expect(page).to have_content(@m_memory.created_at)
-    expect(page).to have_content(@cloth_belt.id)
+    expect(page).to have_content("Updated at: #{@gloves.updated_at}")
+    expect(page).to have_content("Boss ID: #{@feet.world_boss_id}")
+    expect(page).to have_content("Created at: #{@m_memory.created_at}")
+    expect(page).to have_content("ID: #{@cloth_belt.id}")
     expect(page).to have_content("World of Warcraft(Shadowlands) Loot Table")
+  end
+
+  it 'has a link to each Loot' do
+    expect(page).to have_link(@ring.name)
+    expect(page).to have_link(@cloth_belt.name)
+
+    click_link @ring.name
+    expect(current_path).to eq("/loots/#{@ring.id}")
+
+
+    visit "/loots"
+
+    click_link @cloth_belt.name
+    expect(current_path).to eq("/loots/#{@cloth_belt.id}")
+
   end
 end
