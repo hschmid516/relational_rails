151b8fe53b9323608b383d67fb85286c7c18406d
Henry Schmid <henry.schmid1@gmail.com>
2021-08-24 21:03:55 -0600

    Add text to have_content assertions, add link tests

--- spec/features/regions/index_spec.rb
+++ spec/features/regions/index_spec.rb
@@ -4,14 +4,28 @@ RSpec.describe "regions index page", type: :feature do
   before :each do
     @great_plateau = Region.create!(name: "The Great Plateau", has_divine_beast: false, shrines: 4)
     @akkala = Region.create!(name: "Akkala", has_divine_beast: false, shrines: 8)
+    visit "/regions"
   end
 
   it 'can see all regions recorded in the system' do
-    visit "/regions"
-
     expect(current_path).to eq("/regions")
     expect(page).to have_content(@great_plateau.name)
     expect(page).to have_content(@akkala.name)
     expect(page).to have_content("Legend of Zelda: Breath of the Wild - Regions")
+  end
+
+  it 'has a link to each region' do
+    expect(page).to have_link(@great_plateau.name)
+    expect(page).to have_link(@akkala.name)
+
+    click_link @great_plateau.name
+
+    expect(current_path).to eq("/regions/#{@great_plateau.id}")
+
+    visit "/regions"
+
+    click_link @akkala.name
+
+    expect(current_path).to eq("/regions/#{@akkala.id}")
   end
 end
