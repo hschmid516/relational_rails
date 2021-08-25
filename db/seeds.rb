2d8c28c0bb308d3ed23d958678feb18f4916ac61
Henry Schmid <henry.schmid1@gmail.com>
2021-08-24 19:30:06 -0600

    Create seeds for locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 WorldBoss.destroy_all
 Region.destroy_all
+Location.destroy_all
 
 mortanis = WorldBoss.create!(name: "Mortanis", max_health:18_466_000, is_current_boss: false, zone: "Maldraxxus")
-the_great_plateau = Region.create!(name: "The Great Plateau", has_divine_beast: false, shrines: 4)
+great_plateau = Region.create!(name: "The Great Plateau", has_divine_beast: false, shrines: 4)
+akkala = Region.create!(name: "Akkala", has_divine_beast: false, shrines: 8)
+
+akkala.locations.create!(name: "Tarrey Town", is_cold: false, korok_seeds: 13)
+akkala.locations.create!(name: "Lomei Labrynth", is_cold: false, korok_seeds: 5)
+akkala.locations.create!(name: "Akkala Tower", is_cold: true, korok_seeds: 1)
+
+great_plateau.locations.create!(name: "Mount Hylia", is_cold: true, korok_seeds: 11)
+great_plateau.locations.create!(name: "Temple of Time", is_cold: false, korok_seeds: 8)
+great_plateau.locations.create!(name: "Shrine of Resurrection", is_cold: false, korok_seeds: 7)
