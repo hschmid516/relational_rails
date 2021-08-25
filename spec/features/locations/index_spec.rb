6a2ee1d82fde0102787caa25a54129cdad724abc
Henry Schmid <henry.schmid1@gmail.com>
2021-08-24 19:30:28 -0600

    Add tests for locations index

---
+++ spec/features/locations/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "regions index page", type: :feature do
+  before :each do
+    @great_plateau = Region.create!(name: "The Great Plateau", has_divine_beast: false, shrines: 4)
+    @akkala = Region.create!(name: "Akkala", has_divine_beast: false, shrines: 8)
+    @tarrey = @akkala.locations.create!(name: "Tarrey Town", is_cold: false, korok_seeds: 13)
+    @hylia = @great_plateau.locations.create!(name: "Mount Hylia", is_cold: true, korok_seeds: 11)
+  end
+
+  it 'can see all regions recorded in the system' do
+    visit "/locations"
+
+    expect(current_path).to eq("/locations")
+    expect(page).to have_content(@tarrey.name)
+    expect(page).to have_content(@hylia.name)
+    expect(page).to have_content(@tarrey.is_cold)
+    expect(page).to have_content(@hylia.is_cold)
+    expect(page).to have_content(@tarrey.korok_seeds)
+    expect(page).to have_content(@hylia.korok_seeds)
+    expect(page).to have_content("Legend of Zelda: Breath of the Wild - Locations")
+  end
+end
