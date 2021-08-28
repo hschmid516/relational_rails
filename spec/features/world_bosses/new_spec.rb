64c59b2b08ea5bbd4b674163bdc7bd8bb00a1a75
Laura Court <laura.mcourt@gmail.com>
2021-08-28 15:24:21 -0400

    Adjust tests

--- spec/features/world_bosses/new_spec.rb
+++ spec/features/world_bosses/new_spec.rb
@@ -12,14 +12,14 @@ RSpec.describe 'the Boss creation' do
     visit '/world_bosses/new'
 
     fill_in('Name', with: 'Valinor')
-    fill_in('max_health', with: 18000)
+    fill_in('max_health', with: '18000')
     check('is_current_boss')
     fill_in('Zone', with: 'Bastion')
     click_button('Create Boss')
 
     expect(current_path).to eq("/world_bosses")
     expect(page).to have_content('Valinor')
-    expect(page).to have_content(18000)
+    expect(page).to have_content('18000')
     expect(page).to have_content('Bastion')
     expect(page).to have_content('Is current boss?')
   end
--- spec/models/world_bosses_spec.rb
+++ spec/models/world_bosses_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
 describe WorldBoss, type: :model do
-  before(:each) do
-    @mortanis = WorldBoss.create!(name: "Mortanis", max_health:100_000_000, is_current_boss: false, zone: "Maldraxxus")
-    @muckformed = WorldBoss.create!(name: "Muckformed", max_health:19_000_000, is_current_boss: false, zone: "Revendreth")
-    @oranomonos = WorldBoss.create!(name: "Oranomonos the Everbranching", max_health:12_000_000, is_current_boss: true, zone: "Ardenweald")
-
-  end
-
-  it 'exists and has attributes' do
-    expect(@mortanis).to be_a(WorldBoss)
-    expect(@mortanis.name).to eq("Mortanis")
-    expect(@mortanis.max_health).to eq(100_000_000)
-    expect(@mortanis.is_current_boss).to eq(false)
-    expect(@mortanis.zone).to eq("Maldraxxus")
-  end
-
-  it 'has world bosses ordered by name' do
-
-    expected = [@oranomonos, @muckformed, @mortanis]
-    expect(WorldBoss.ordered_bosses).to eq(expected)
-  end
+  it {should have_many :loots}
 end
