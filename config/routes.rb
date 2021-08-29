cf778bc2c7590f79c9c2fa246641ed9c165bb6b2
Henry Schmid <henry.schmid1@gmail.com>
2021-08-29 14:17:07 -0600

    Add features to delete regions and locations

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,13 @@ Rails.application.routes.draw do
   patch '/regions/:id', to: 'regions#update'
   get '/regions/:id/locations/new', to: 'region_locations#new'
   post '/regions/:id/locations', to: 'region_locations#create'
+  delete '/regions/:id', to: 'regions#destroy'
 
   get '/locations', to: 'locations#index'
   get '/locations/:id', to: 'locations#show'
   get '/locations/:id/edit', to: 'locations#edit'
   patch '/locations/:id', to: 'locations#update'
+  delete '/locations/:id', to: 'locations#destroy'
 
   get '/loots', to: 'loots#index'
   get '/loots/:id', to: 'loots#show'
