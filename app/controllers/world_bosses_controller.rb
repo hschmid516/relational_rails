fef22320ba1ed260145024656c336e8a6465c246
Laura Court <laura.mcourt@gmail.com>
2021-08-31 18:31:37 -0400

    Refactor

--- app/controllers/world_bosses_controller.rb
+++ app/controllers/world_bosses_controller.rb
@@ -1,22 +1,14 @@
 class WorldBossesController < ApplicationController
+  before_action :find_by_id, only: %i[show edit update]
   def index
-    @world_bosses = WorldBoss.exact_search(params[:search_exact]).partial_search(params[:search_partial]).ordered_bosses(params[:sort])
-  end
-
-  def show
-    @world_boss = WorldBoss.find(params[:id])
-  end
-
-  def new
+    @world_bosses = WorldBoss.exact_search(params[:search_exact])
+                            .partial_search(params[:search_partial])
+                            .ordered_bosses(params[:sort])
   end
 
   def create
     world_boss = WorldBoss.create(boss_params)
     redirect_to '/world_bosses'
-  end
-
-  def edit
-    @world_boss = WorldBoss.find(params[:id])
   end
 
   def update
@@ -33,5 +25,9 @@ class WorldBossesController < ApplicationController
 private
   def boss_params
     params.permit(:name, :max_health, :zone, :is_current_boss)
+  end
+
+  def find_by_id
+    @world_boss = WorldBoss.find(params[:id])
   end
 end
