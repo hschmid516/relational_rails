22d65b40a86c39946ce90bc69203f3b8ca6ede1a
Henry Schmid <henry.schmid1@gmail.com>
2021-08-31 14:52:05 -0600

    Move search scopes to ApplicationRecord

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,3 @@
 class Location < ApplicationRecord
   belongs_to :region
-
-  scope :exact_search, ->(search) { where(name: search) if search != nil}
-
-  scope :partial_search, ->(search) { where("name ilike ?", "%#{search}%") if search != nil}
 end
--- app/models/world_boss.rb
+++ app/models/world_boss.rb
@@ -25,7 +25,4 @@ class WorldBoss < ApplicationRecord
     armor = 0 if armor == nil
     loots.where("armor > #{armor}")
   end
-
-  scope :exact_search, ->(search) {where(name: search) if search != nil}
-  scope :partial_search, ->(search) { where("name ilike ?", "%#{search}%") if search != nil}
 end
