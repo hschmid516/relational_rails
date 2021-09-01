d7a589426e9b06162a84b628d841bb162b0d1619
Henry Schmid <henry.schmid1@gmail.com>
2021-08-31 21:33:58 -0600

    Add factories to tests, add within blocks

--- spec/models/regions_spec.rb
+++ spec/models/regions_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Region, type: :model do
+  it { validate_presence_of :name }
+  it { validate_presence_of :shrines }
+
   it { should have_many :locations }
 
   before(:each) do
